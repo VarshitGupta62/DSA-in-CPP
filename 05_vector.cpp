2a2d54d7dda65eb552473aa6fbcdac6aed756082
Varshit Gupta <varshitgupta45@gmail.com>
2026-05-24 22:26:37 +0530

    n number sum

--- 05_vector.cpp
+++ 05_vector.cpp
@@ -1,15 +1,34 @@
 // Take n numbers in vector and print sum.
 
-#include <isotream>
+#include <iostream>
 #include <vector>
+using namespace std;
 
 int main(){
 
     int n;
-    int x;
+    vector<int> v;
 
     cout<<"Enter the size of vector : "<<endl;
-    cin>>n
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+
+        int x;
+        cout<<"Enter numer is "<<i + 1<<" : ";
+        cin>>x;
+        v.push_back(x);
+        
+
+    }
+
+    int sum = 0;
+
+    for(int element : v){
+        sum = sum + element;
+    }
+
+    cout<<"Sum is : "<<sum;
 
     return 0;
 }
