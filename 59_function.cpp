0d19c7966d66462c37e33085c591226a41c1e618
Varshit Gupta <varshitgupta45@gmail.com>
2026-06-14 16:02:19 +0530

    function

--- 59_function.cpp
+++ 59_function.cpp
@@ -3,36 +3,38 @@
 
 using namespace std;
 
-class  BankAccount{
-    public:
+
+class BankAccount
+{
+
+public:
+
     string name;
     string accountNumber;
     double balance;
 
-    void addAccount(string n , string an , double b){
+
+    void createAccount(string n, string an, double b)
+    {
         name = n;
         accountNumber = an;
         balance = b;
     }
 
-    void display(){
-        cout<<"Account Holder Name is : "<<name<<endl;
-        cout<<"Account Number is : "<<accountNumber<<endl;
-        cout<<"Total Balance is : "<<balance<<endl;
-    }
 
-     // Deposit
+
     void deposit(double amount)
     {
         balance += amount;
     }
 
-    // Function Overloading
+
 
     double calculateInterest()
     {
         return balance * 0.05;
     }
+
 
 
     double calculateInterest(double rate)
@@ -41,33 +43,70 @@ class  BankAccount{
     }
 
 
+
+    void display()
+    {
+        cout<<"Name: "<<name<<endl;
+        cout<<"Account No: "<<accountNumber<<endl;
+        cout<<"Balance: "<<balance<<endl;
+
+        cout<<"------------------"<<endl;
+    }
+
+
 };
 
-int main(){
+
+
+int main()
+{
 
     vector<BankAccount> accounts;
 
+
     BankAccount A1;
 
-    A1.addAccount("Varshit Gupta", "E213012" , 12000);
+
+    A1.createAccount(
+        "Varshit Gupta",
+        "E213012",
+        12000
+    );
+
 
     A1.deposit(50000);
 
-    double intrest = A1.calculateInterest(10);
 
-    A1.deposit(intrest);
+    double interest = A1.calculateInterest(10);
+
+
+    A1.deposit(interest);
+
+
 
     BankAccount A2;
 
-    A2.addAccount("Harshit Gupta", "E213013" , 56000);
+
+    A2.createAccount(
+        "Harshit Gupta",
+        "E213013",
+        56000
+    );
+
+
 
     accounts.push_back(A1);
+
     accounts.push_back(A2);
 
-    for(auto element:accounts){
+
+
+    for(auto element : accounts)
+    {
         element.display();
     }
 
-    
+
+    return 0;
 
 }
