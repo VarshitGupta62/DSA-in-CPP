db3ad5d00611ce2c02ea8563bcb606e855a293f5
Varshit Gupta <varshitgupta45@gmail.com>
2026-06-15 16:09:53 +0530

    pointer

---
+++ 61_pointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 10;
+
+    int *ptr = &a;
+
+    cout << a << endl;
+
+    cout << *ptr<<endl;
+
+    *ptr = 20;
+
+    cout << *ptr<<endl<<a;
+}
