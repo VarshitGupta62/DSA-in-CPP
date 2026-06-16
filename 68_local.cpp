bacd4778eb37dd88a4514e9863aa66c26d029cc3
Varshit Gupta <varshitgupta45@gmail.com>
2026-06-16 12:14:34 +0530

    scope

---
+++ 68_local.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+void test()
+{
+    int x = 10;
+
+    cout<<x;
+}
+
+
+int main()
+{
+    test();
+
+    // cout<<x;  // Error
+}
---
+++ 70_global.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int value = 100;
+
+
+void show()
+{
+    cout<<value<<endl;
+}
+
+
+int main()
+{
+    show();
+
+    cout<<value;
+}
