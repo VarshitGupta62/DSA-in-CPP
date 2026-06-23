89b3013081d262a344393050951ebedf273771d7
Varshit Gupta <varshitgupta45@gmail.com>
2026-06-23 15:56:14 +0530

    algo

--- 01_algo.cpp
+++ 01_algo.cpp
@@ -1,29 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void bubbleSort(int arr[], int n)
 {
-    for(int i=0;i<n-1;i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int j=0;j<n-i-1;j++)
+        cout<<"value is i : "<<i<<endl;
+
+        bool s = false;
+
+
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if(arr[j] > arr[j+1])
+            if (arr[j] > arr[j + 1])
             {
-                swap(arr[j], arr[j+1]);
+                swap(arr[j], arr[j + 1]);
+
+                 for(int k = 0; k < n; k++)
+                    cout << arr[k] << " ";
+
+                cout << endl;
+
+                s = true;
             }
         }
+
+        if(s == false){
+
+            break;
+            
+        }
+
+
     }
 }
 
 int main()
 {
-    int arr[] = {5,3,8,1,2};
+    int arr[] = {5, 3, 8, 1, 2};
+
     int n = 5;
+     cout<<"Before short : ";
+    
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    
+    cout<<endl;
 
-    bubbleSort(arr,n);
+    bubbleSort(arr, n);
 
-    for(int i=0;i<n;i++)
-        cout<<arr[i]<<" ";
+    // for (int i = 0; i < n; i++)
+    //     cout << arr[i] << " ";
 
     return 0;
 }
