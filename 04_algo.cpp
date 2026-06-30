ab8bc004839740f73255cc25faccbfbce9681efd
Varshit Gupta <varshitgupta45@gmail.com>
2026-06-30 17:27:51 +0530

    algo

--- 04_algo.cpp
+++ 04_algo.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int partition(int arr[], int low, int high)
 {
-    int pivot = arr[high];   // choose last element as pivot
+    int pivot = arr[high]; // choose last element as pivot
     int i = low - 1;
 
-    for(int j = low; j < high; j++)
+    for (int j = low; j < high; j++)
     {
-        if(arr[j] < pivot)
+        if (arr[j] < pivot)
         {
             i++;
             swap(arr[i], arr[j]);
@@ -20,27 +20,45 @@ int partition(int arr[], int low, int high)
     return i + 1;
 }
 
-
 void quickSort(int arr[], int low, int high)
 {
-    if(low < high)
+    if (low < high)
     {
         int pivotIndex = partition(arr, low, high);
+        // cout << "Array after partition: ";
 
-        quickSort(arr, low, pivotIndex - 1);   // left part
-        quickSort(arr, pivotIndex + 1, high);  // right part
+        // for (int l = 0; l < 5; l++)
+        // {
+        //     cout << arr[l] << " ";
+        // }
+
+        quickSort(arr, low, pivotIndex - 1);
+
+        // cout << "Array left: ";
+
+        // for (int l = 0; l < 5; l++)
+        // {
+        //     cout << arr[l] << " ";
+        // }
+        // quickSort(arr, pivotIndex + 1, high);
+
+        // cout << "Array left: ";
+
+        // for (int l = 0; l < 5; l++)
+        // {
+        //     cout << arr[l] << " ";
+        // }
     }
 }
-
 
 int main()
 {
     int arr[] = {5, 2, 8, 1, 3};
     int n = 5;
 
-    quickSort(arr, 0, n-1);
+    quickSort(arr, 0, n - 1);
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
