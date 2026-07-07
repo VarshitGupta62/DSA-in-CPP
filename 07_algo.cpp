2dc63e84ab45cb28933cea500802b70bbfca5eba
Varshit Gupta <varshitgupta45@gmail.com>
2026-07-07 18:04:13 +0530

    algo

---
+++ 07_algo.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid; // Element found
+        }
+        else if (arr[mid] < key)
+        {
+            low = mid + 1; // Search in right half
+        }
+        else
+        {
+            high = mid - 1; // Search in left half
+        }
+    }
+
+    return -1; // Element not found
+}
+
+int main()
+{
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int key = 23;
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found." << endl;
+
+    return 0;
+}
